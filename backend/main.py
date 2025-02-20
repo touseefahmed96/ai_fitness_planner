7043310d0b52d52d3a32c5fc8ffc8bb1405efb78
Touseef Ahmad <touseefahmed9669@gmail.com>
2025-02-20 12:27:37 +0500

    Routes added for user workout and diet

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from routes import diet, user, workout
 
 app = FastAPI(title="AI Workout & Diet Planner", version="1.0")
+
+
+# Include routes
+app.include_router(user.router, prefix="/user", tags=["User"])
+app.include_router(workout.router, prefix="/workout", tags=["Workout"])
+app.include_router(diet.router, prefix="/diet", tags=["Diet"])
 
 
 @app.get("/")
