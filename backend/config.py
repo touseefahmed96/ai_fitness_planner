86a148a3cf434b45928650f8bd16bf1eb6d2d7be
Touseef Ahmad <touseefahmed9669@gmail.com>
2025-02-20 12:08:31 +0500

    - Added database configuration with SQLAlchemy.
    - Config file for all the secret keys

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    DATABASE_URL = os.getenv("DATABASE_URL", "")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+    SECRET_KEY = os.getenv("SECRET_KEY", "")
+    ALGORITHM = os.getenv("ALGORITHM", "")
